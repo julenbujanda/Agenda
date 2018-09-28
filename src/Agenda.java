0f4d281bebd5172b2e89832d25bb3fa52301156d
julenb <21701719@live.uem.es>
2018-09-28 20:27:46 +0200

    Comparador

--- src/Agenda.java
+++ src/Agenda.java
@@ -1,12 +1,18 @@
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 
-public class Agenda<Contacto> extends LinkedList {
+public class Agenda<E> extends LinkedList {
 
     @Override
     public boolean add(Object o) {
         boolean resultado = super.add(o);
-        Collections.sort(this);
+        Collections.sort(this, new Comparator<Contacto>() {
+            @Override
+            public int compare(Contacto o1, Contacto o2) {
+                return o1.getNombre().compareToIgnoreCase(o2.getNombre());
+            }
+        });
         return resultado;
     }
 }
